feb28a7c01c83bc08a8c4fc6bce0f81580637996
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-01 18:17:34 -0600

    Add not found page and cleanup root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="home" />
-      <Stack.Screen name="records" />
-      <Stack.Screen name="calculators" />
+    <Stack>
+      <Stack.Screen name="(home)" />
     </Stack>
   );
 }
