6a9bdf767bd889089681679619af508823a882f1
Kevin Tatooles <kevintatooles@gmail.com>
2025-01-02 22:41:53 -0600

    Styling for tab layout

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+export default function HomeLayout() {
   return (
     <Stack
       screenOptions={{
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,44 @@
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="(home)" />
-      <Tabs.Screen name="workouts" />
-      <Tabs.Screen name="exercises" />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "teal" }}>
+      <Tabs.Screen
+        name="(home)"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="workouts"
+        options={{
+          title: "Workouts",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="weight-lifter"
+              size={28}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="exercises"
+        options={{
+          title: "Exercises",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="barbell" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
