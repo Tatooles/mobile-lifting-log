b4bacf56cf895dcca2e50a9a341ee5e97aba23c4
Kevin Tatooles <kevintatooles@gmail.com>
2025-04-23 22:39:17 -0500

    Adjust styling of workout list

--- app/(tabs)/workouts/workout-box.tsx
+++ app/(tabs)/workouts/workout-box.tsx
@@ -12,7 +12,7 @@ const WorkoutBox = ({
   minutes: number;
 }) => {
   return (
-    <View className="bg-gray-100 rounded-lg p-4 m-4 shadow-md">
+    <View className="bg-gray-100 p-4 shadow-md w-11/12 flex-col items-center">
       <Text className="text-sm text-gray-600 mb-1">{date}</Text>
       <Text className="text-lg font-bold mb-2">{name}</Text>
       <Text className="text-base text-gray-700">
