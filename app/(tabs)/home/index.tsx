d4a2d30c1f229fb535e8456fa8997e25d67aeca8
Kevin Tatooles <kevintatooles@gmail.com>
2025-04-27 18:39:24 -0500

    Add dummy data insert and fetch data on home page

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,7 +1,24 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { useEffect, useState } from "react";
+import { useSQLiteContext } from "expo-sqlite";
+import * as schema from "@/db/schema";
+import { drizzle } from "drizzle-orm/expo-sqlite";
 
 export default function HomeScreen() {
+  const [data, setData] = useState([]);
+
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db, { schema });
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await drizzleDb.query.workout.findMany();
+      console.log("~ load ~ data:", data);
+      // setData(data);
+    };
+    load();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Home</Text>
