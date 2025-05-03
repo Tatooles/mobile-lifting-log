aa251547213147753219efb95f7a91db2f78d694
Kevin Tatooles <kevintatooles@gmail.com>
2025-05-02 23:05:58 -0500

    Add logic for querying workout details, and display raw JSON

--- app/(tabs)/workouts/details/[id].tsx
+++ app/(tabs)/workouts/details/[id].tsx
@@ -1,12 +1,43 @@
+import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useLocalSearchParams } from "expo-router";
+import { useSQLiteContext } from "expo-sqlite";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { Workout } from "~/lib/types";
+import * as schema from "@/db/schema";
 
 export default function WorkoutDetails() {
   const { id } = useLocalSearchParams();
 
+  const [workout, setWorkout] = useState<Workout>();
+
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db, { schema });
+
+  useEffect(() => {
+    (async () => {
+      const data: Workout | undefined = await drizzleDb.query.workout.findFirst(
+        {
+          where: (workout, { eq }) => eq(workout.id, +id),
+          with: {
+            exercises: {
+              with: {
+                sets: {
+                  orderBy: (sets, { asc }) => [asc(sets.id)],
+                },
+              },
+            },
+          },
+        }
+      );
+      setWorkout(data);
+    })();
+  }, []);
+
   return (
     <View className="items-center">
-      <Text>Details page for workout {id}</Text>
+      <Text>Details page for workout</Text>
+      <Text>{JSON.stringify(workout)}</Text>
     </View>
   );
 }
