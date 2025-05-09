53a9db3eafe0404614c31e95ee565fad3fc2a429
Kevin Tatooles <kevintatooles@gmail.com>
2025-05-09 17:55:29 -0500

    Start adding styling/logic for swipe to delete

--- app/(tabs)/workouts/workout-box.tsx
+++ app/(tabs)/workouts/workout-box.tsx
@@ -1,7 +1,28 @@
 import { Link } from "expo-router";
-import { Text, Pressable } from "react-native";
+import { Text, Pressable, View } from "react-native";
 import { formatDate } from "~/lib/dateUtils";
 import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import Reanimated, {
+  SharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+function RightAction(prog: SharedValue<number>, drag: SharedValue<number>) {
+  const styleAnimation = useAnimatedStyle(() => {
+    console.log("showRightProgress:", prog.value);
+    console.log("appliedTranslation:", drag.value);
+
+    return {
+      transform: [{ translateX: drag.value + 50 }],
+    };
+  });
+
+  return (
+    <Reanimated.View style={styleAnimation}>
+      <Text className="h-full bg-red-700">Delete</Text>
+    </Reanimated.View>
+  );
+}
 
 const WorkoutBox = ({
   id,
@@ -17,7 +38,7 @@ const WorkoutBox = ({
   // Look through more apple apps to try to make this as native as possible
   // Fitness app is a decent start
   return (
-    <Swipeable>
+    <Swipeable renderRightActions={RightAction}>
       <Link href={`/workouts/details/${id}`} asChild>
         <Pressable className="bg-gray-300 p-4 mx-4 my-1 rounded-lg flex-col items-center">
           <Text className="text-sm text-gray-600 mb-1">{formatDate(date)}</Text>
