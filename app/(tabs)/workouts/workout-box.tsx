c555517a5b970701f9c71cdc82b4f863276717f4
Kevin Tatooles <kevintatooles@gmail.com>
2025-05-08 22:44:38 -0500

    Use react-native-gesture handler to allow swiping of workout box

--- app/(tabs)/workouts/workout-box.tsx
+++ app/(tabs)/workouts/workout-box.tsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
 import { Text, Pressable } from "react-native";
 import { formatDate } from "~/lib/dateUtils";
+import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
 
 const WorkoutBox = ({
   id,
@@ -16,17 +17,17 @@ const WorkoutBox = ({
   // Look through more apple apps to try to make this as native as possible
   // Fitness app is a decent start
   return (
-    // TODO: Apple Fitness app allows the user to slide to delete with a modal for confirmation
-    // Should try to replicate that
-    <Link href={`/workouts/details/${id}`} asChild>
-      <Pressable className="bg-gray-300 p-4 mx-4 my-1 rounded-lg flex-col items-center">
-        <Text className="text-sm text-gray-600 mb-1">{formatDate(date)}</Text>
-        <Text className="text-lg font-bold mb-2">{name}</Text>
-        <Text className="text-base text-gray-700">
-          Exercises ({exerciseCount})
-        </Text>
-      </Pressable>
-    </Link>
+    <Swipeable>
+      <Link href={`/workouts/details/${id}`} asChild>
+        <Pressable className="bg-gray-300 p-4 mx-4 my-1 rounded-lg flex-col items-center">
+          <Text className="text-sm text-gray-600 mb-1">{formatDate(date)}</Text>
+          <Text className="text-lg font-bold mb-2">{name}</Text>
+          <Text className="text-base text-gray-700">
+            Exercises ({exerciseCount})
+          </Text>
+        </Pressable>
+      </Link>
+    </Swipeable>
   );
 };
 
