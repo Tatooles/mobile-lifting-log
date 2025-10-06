59d022ceb613fae92b49d9d711d233e2cedf6327
Kevin Tatooles <kevintatooles@gmail.com>
2025-10-05 22:00:12 -0500

    Change add workout button to green and add dark mode for workout details page

--- app/(workouts)/details/[id].tsx
+++ app/(workouts)/details/[id].tsx
@@ -39,12 +39,12 @@ export default function WorkoutDetails() {
   const formattedDate = formatDate(workout?.date);
 
   return (
-    <ScrollView className="flex-1">
+    <ScrollView className="flex-1 bg-gray-50 dark:bg-gray-900">
       <Stack.Screen options={{ title: `${formattedDate}` }} />
 
       {/* Header */}
       <View className="px-4 pt-4 flex-row items-center">
-        <Text className="text-2xl font-bold text-gray-800 flex-1">
+        <Text className="text-2xl font-bold text-gray-800 dark:text-white flex-1">
           {workout?.name}
         </Text>
         {/* Would be nice to have the duration of the workout here */}
@@ -52,21 +52,21 @@ export default function WorkoutDetails() {
 
       {/* Exercises */}
       <View className="px-4 py-4">
-        <Text className="text-lg font-semibold text-gray-800 mb-4">
+        <Text className="text-lg font-semibold text-gray-800 dark:text-white mb-4">
           Exercises ({workout?.exercises.length})
         </Text>
 
         {workout?.exercises.map((exercise, index) => (
           <View
             key={exercise.id}
-            className={`bg-gray-50 rounded-xl p-4 mb-4 ${
+            className={`bg-gray-50 dark:bg-gray-800 rounded-xl p-4 mb-4 ${
               index === workout.exercises.length - 1 ? "" : "mb-4"
             }`}
           >
             <View className="flex-row items-center mb-2">
               <Dumbbell size={20} color="#4B5563" />
               {/* This will eventually need to be clickable to see the history of this exercise */}
-              <Text className="ml-2 text-lg font-semibold text-gray-800">
+              <Text className="ml-2 text-lg font-semibold text-gray-800 dark:text-white">
                 {exercise.name}
               </Text>
             </View>
@@ -74,7 +74,7 @@ export default function WorkoutDetails() {
             {exercise.notes ? (
               <View className="flex-row items-start mb-3">
                 <ClipboardList size={16} color="#6B7280" className="mt-1" />
-                <Text className="ml-2 text-gray-600 flex-1">
+                <Text className="ml-2 text-gray-600 dark:text-gray-300 flex-1">
                   {exercise.notes}
                 </Text>
               </View>
@@ -82,15 +82,17 @@ export default function WorkoutDetails() {
 
             {/* Sets */}
             <View className="mt-2">
-              <View className="flex-row bg-gray-200 rounded-t-lg p-3">
-                <Text className="font-medium text-gray-700 flex-1">Set</Text>
-                <Text className="font-medium text-gray-700 w-20 text-center">
+              <View className="flex-row bg-gray-200 dark:bg-gray-700 rounded-t-lg p-3">
+                <Text className="font-medium text-gray-700 dark:text-gray-200 flex-1">
+                  Set
+                </Text>
+                <Text className="font-medium text-gray-700 dark:text-gray-200 w-20 text-center">
                   Weight
                 </Text>
-                <Text className="font-medium text-gray-700 w-20 text-center">
+                <Text className="font-medium text-gray-700 dark:text-gray-200 w-20 text-center">
                   Reps
                 </Text>
-                <Text className="font-medium text-gray-700 w-16 text-center">
+                <Text className="font-medium text-gray-700 dark:text-gray-200 w-16 text-center">
                   RPE
                 </Text>
               </View>
@@ -99,21 +101,25 @@ export default function WorkoutDetails() {
                 <View
                   key={set.id}
                   className={`flex-row p-3 ${
-                    setIndex % 2 === 0 ? "bg-white" : "bg-gray-50"
+                    setIndex % 2 === 0
+                      ? "bg-white dark:bg-gray-800"
+                      : "bg-gray-50 dark:bg-gray-700"
                   } ${
                     setIndex === exercise.sets.length - 1
                       ? "rounded-b-lg"
-                      : "border-b border-gray-100"
+                      : "border-b border-gray-100 dark:border-gray-600"
                   }`}
                 >
-                  <Text className="text-gray-800 flex-1">{setIndex + 1}</Text>
-                  <Text className="text-gray-800 w-20 text-center">
+                  <Text className="text-gray-800 dark:text-white flex-1">
+                    {setIndex + 1}
+                  </Text>
+                  <Text className="text-gray-800 dark:text-white w-20 text-center">
                     {set.weight}
                   </Text>
-                  <Text className="text-gray-800 w-20 text-center">
+                  <Text className="text-gray-800 dark:text-white w-20 text-center">
                     {set.reps}
                   </Text>
-                  <Text className="text-gray-800 w-16 text-center">
+                  <Text className="text-gray-800 dark:text-white w-16 text-center">
                     {set.rpe || "-"}
                   </Text>
                 </View>
