8e26a50bd6ce6c15d860cfb16956a9f3d3634755
Kevin Tatooles <kevintatooles@gmail.com>
2025-11-17 23:02:54 -0600

    Use SafeAreaProvider properly to try to fix warning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function MainLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <ClerkLoaded>
-        <Slot />
-      </ClerkLoaded>
-    </ClerkProvider>
+    <SafeAreaProvider>
+      <ClerkProvider tokenCache={tokenCache}>
+        <ClerkLoaded>
+          <Slot />
+        </ClerkLoaded>
+      </ClerkProvider>
+    </SafeAreaProvider>
   );
 }
